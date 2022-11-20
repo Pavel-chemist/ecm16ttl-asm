7225c52121d9ea24651a637d119de86b473c5813
Pavel-chemist <pavel.chemist@gmail.com>
2022-11-20 20:45:29 +0400

    add input from file

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,53 @@ mod command_interpreter;
 // use helpers::{read_input, number_parser};
 use command_interpreter::command_interpreter;
 
+use std::fs;
+// use fltk::{app, prelude::*, *, enums};
+
+
 fn main() {
     let mut command: String;
     let mut program: Vec<u16> = Vec::new();
     let mut machine_instructions: Vec<u16>;
+    let file_path: String;
+    let file_contents: String;
+    let mut code_lines: Vec<&str> = Vec::new();
 
-    println!("Enter asm commands:");
 
-    loop {
+    println!("Enter the path to the file: ");
+    file_path = helpers::read_input("this should be a string");
+
+    match fs::read_to_string(file_path) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(buff) => {
+            file_contents = buff;
+            code_lines = file_contents.lines().collect();
+
+            println!("lines: ");
+
+            for i in 0..code_lines.len() {
+                println!("{}", code_lines[i]);
+            }
+        }
+    };
+
+    for i in 0..code_lines.len() {
+        machine_instructions = command_interpreter(code_lines[i]);
+        
+        if machine_instructions[0] != 0x00FF {
+            for i in 0..machine_instructions.len() {
+                program.push(machine_instructions[i]);
+            }
+        }
+    }
+
+    // println!("Enter asm commands:");
+
+    /* loop {
         command = helpers::read_input("");
         if command.len() > 0 {
 
             machine_instructions = command_interpreter(&command);
-
-            // println!("number is {:X}", helpers::number_parser(&command) as u16);
         
             if machine_instructions[0] != 0x00FF {
                 for i in 0..machine_instructions.len() {
@@ -31,7 +64,7 @@ fn main() {
         } else {
             break;
         }
-    }   
+    } */   
 
     show_code(program);
 }
@@ -57,5 +90,52 @@ fn show_code(instructions: Vec<u16>) {
     }
 }
 
+/* 
+fn open_file_dialog() /* -> Vec<String> */ {
+    let mut dialog = dialog::NativeFileChooser::new(dialog::NativeFileChooserType::BrowseFile);
+    dialog.show();
+
+    println!("{:?}", dialog.filename());
+
+    let file_name: String = dialog.filename().into_os_string().into_string().unwrap();
+
+    let mut string_with_newlines: String = String::new();
+
+    for c in file_name.chars() {
+        if c == '/' {
+            string_with_newlines.push('\n');
+        } else {
+            string_with_newlines.push(c);
+        }
+    }
+    
+    if file_name.len() > 0 {
+        // return read_file(&file_name);
+
+        read_file(&file_name);
+    }
+
+    // return vec![String::from("No file chosen"); 1];
+}
+
+fn read_file(file_path: &str) /* -> Vec<String> */ {
+    let mut file_buff: Vec<u8> = Vec::new();
+
+    match fs::read(file_path) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(buff) => {
+            file_buff = buff;
+
+            println!("file buffer:");
+
+            for i in 0..file_buff.len() {
+                print!("{}, ", file_buff[i]);
+            }
+        }
+    };
 
 
+
+    // return print_file_buffer(file_buff);
+}
+ */
---
+++ test/test.asm
@@ -0,0 +1,2 @@
+ADDi r1 0x1F
+LD r2 (SP) [-16]
